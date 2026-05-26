cc750870cec2cd8b9010b046d6de55dcc7c51a9e
Pranay New <pranay@friendlydiamonds.com>
2026-05-26 18:50:26 +0530

    metal validate code issue

--- FD/tests/test_ring_setting_metals.py
+++ FD/tests/test_ring_setting_metals.py
@@ -1,10 +1,22 @@
-from FD.pages.metal_swatch_verify_on_pdp.ring_setting_page import RingSettingPage
+import pytest
+
+from FD.pages.metal_swatch_verify_on_pdp.ring_setting_page import (
+    RingSettingPage
+)
 
 
+@pytest.mark.regression
 class TestRingSettingMetals:
 
     def test_verify_all_products_all_metals(self, page):
 
         ring_page = RingSettingPage(page)
 
-        ring_page.verify_all_products_metals()
+        failed = (
+            ring_page.verify_all_products_metals()
+        )
+
+        assert not failed, (
+            f"\nTOTAL FAILURES: {len(failed)}"
+            f"\nFAIL DETAILS: {failed}"
+        )
